14f863abb6223228c2e0b0eea9879aa859693d00
lucita <826648003@qq.com>
2015-11-08 00:45:07 +0800

    Initial commit

---
+++ app/src/androidTest/java/stu07140533/lucita/edu/cn/gdmec/work4/ApplicationTest.java
@@ -0,0 +1,13 @@
+package stu07140533.lucita.edu.cn.gdmec.work4;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
